50eebfdfa92cef7ebacaa2d6b3bf6314156325c8
Ben Baldwin <baldwin.ben55@gmail.com>
2023-05-08 20:19:26 -0700

    styled basemap layers, styled geoCode results, styled buttons, changed default basemap

--- client/src/components/BasemapButton.jsx
+++ client/src/components/BasemapButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+const BasemapButton = ({ buttonText, layerParameter, submitFunction, img }) => {
 
-const BasemapButton = ({buttonText, layerParameter, submitFunction}) => {
-
-    return (
-      <button className='border rounded-md p-1 bg-slate-400 active:accent-gray-800' value={layerParameter} onClick={submitFunction}>{buttonText}</button>
-    )
-  }
+  return (
+    <div className='border border-lime-200 p-1 rounded-md bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 h-20 w-20'>
+      <button className='w-full h-full' onClick={() => submitFunction(layerParameter)} id={layerParameter}><img className='w-full h-full rounded-md' src={img} alt="Base Map" /></button>
+      <p className='text-lime-200 text-center text-sm'>{buttonText}</p>
+    </div>
+    // <button className='border rounded-md bg-lime-200 active:accent-gray-800 h-16 w-16' value={layerParameter} img={img} onClick={submitFunction}>{buttonText}</button>
+  )
+}
 
 export default BasemapButton
