4ba62b1ea3189c79dac6dd949bfbd5f73731579f
Ben Baldwin <baldwin.ben55@gmail.com>
2023-05-12 20:48:56 -0700

    reworking design to be more responsive and user friendly at all screen sizes

--- client/src/components/BasemapButton.jsx
+++ client/src/components/BasemapButton.jsx
@@ -4,7 +4,7 @@ const BasemapButton = ({ buttonText, layerParameter, submitFunction, img }) => {
   return (
     <div className='border border-lime-200 p-1 rounded-md bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 h-20 w-20'>
       <button className='w-full h-full' onClick={() => submitFunction(layerParameter)} id={layerParameter}><img className='w-full h-full rounded-md' src={img} alt="Base Map" /></button>
-      <p className='text-lime-200 text-center text-sm'>{buttonText}</p>
+      <p className='text-lime-200 text-center text-xs md:text-sm'>{buttonText}</p>
     </div>
     // <button className='border rounded-md bg-lime-200 active:accent-gray-800 h-16 w-16' value={layerParameter} img={img} onClick={submitFunction}>{buttonText}</button>
   )
