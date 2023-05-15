b0341b035a171e6063408eb19a9f0fb31d0c9b52
Ben Baldwin <baldwin.ben55@gmail.com>
2023-05-14 22:13:30 -0700

    finishing mobile styling

--- client/src/components/BasemapButton.jsx
+++ client/src/components/BasemapButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const BasemapButton = ({ buttonText, layerParameter, submitFunction, img }) => {
 
   return (
-    <div className='border border-lime-200 p-1 rounded-md bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 h-20 w-20'>
+    <div className='border border-lime-200 p-1 rounded-md bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 h-16 w-16 md:h-20 md:w-20'>
       <button className='w-full h-full' onClick={() => submitFunction(layerParameter)} id={layerParameter}><img className='w-full h-full rounded-md' src={img} alt="Base Map" /></button>
       <p className='text-lime-200 text-center text-xs md:text-sm'>{buttonText}</p>
     </div>
